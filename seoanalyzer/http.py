6182f45aaf6721e2612c2fd5dcaa8a3e59f173ab
Burak <burakkiran95@gmail.com>
2020-10-01 10:50:03 -0700

    Added User Agent
    Added a user agent as certain sites return a 400 response page without one.

--- seoanalyzer/http.py
+++ seoanalyzer/http.py
@@ -4,10 +4,12 @@ import urllib3
 
 class Http():
     def __init__(self):
+        user_agent = {'User-Agent': 'Mozilla/5.0'}
         self.http = urllib3.PoolManager(
             timeout=urllib3.Timeout(connect=1.0, read=2.0),
             cert_reqs='CERT_REQUIRED',
-            ca_certs=certifi.where()
+            ca_certs=certifi.where(),
+            headers=user_agent
         )
 
     def get(self, url):
