787445f734a98b91a8bdcb4ae58eb64dfcf8f07e
Seth Black <sblack@sethserver.com>
2021-01-12 11:26:57 -0600

    Remove social share stats

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -3,4 +3,6 @@ from seoanalyzer import analyze
 def test_print_output():
     output = analyze('https://www.sethserver.com/tests/utf8.html')
 
-    assert output
+    print(output)
+
+    assert False
