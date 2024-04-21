067fe020b87c9e7c23798d3da46e8ff65661d173
Seth Black <sblack@sethserver.com>
2024-04-21 13:04:46 -0500

    Refactor everything into pyseoanalyzer directory

--- pyseoanalyzer/http.py
+++ pyseoanalyzer/http.py
@@ -3,18 +3,19 @@ from urllib3 import PoolManager
 from urllib3 import Timeout
 
 
-class Http():
+class Http:
     def __init__(self):
-        user_agent = {'User-Agent': 'Mozilla/5.0'}
+        user_agent = {"User-Agent": "Mozilla/5.0"}
+
         self.http = PoolManager(
             timeout=Timeout(connect=1.0, read=2.0),
-            cert_reqs='CERT_REQUIRED',
+            cert_reqs="CERT_REQUIRED",
             ca_certs=certifi.where(),
-            headers=user_agent
+            headers=user_agent,
         )
 
     def get(self, url):
-        return self.http.request('GET', url)
+        return self.http.request("GET", url)
+
 
 http = Http()
-        
--- tests/test_page.py
+++ tests/test_page.py
@@ -1,20 +1,27 @@
-from seoanalyzer import page
+from pyseoanalyzer import page
+
 
 def test_page_init():
-    p = page.Page(url='https://www.sethserver.com/sitemap.xml', base_domain='https://www.sethserver.com/')
+    p = page.Page(
+        url="https://www.sethserver.com/sitemap.xml",
+        base_domain="https://www.sethserver.com/",
+    )
 
-    assert p.base_domain.scheme == 'https'
-    assert p.base_domain.netloc == 'www.sethserver.com'
-    assert p.base_domain.path == '/'
+    assert p.base_domain.scheme == "https"
+    assert p.base_domain.netloc == "www.sethserver.com"
+    assert p.base_domain.path == "/"
 
-    assert p.url == 'https://www.sethserver.com/sitemap.xml'
+    assert p.url == "https://www.sethserver.com/sitemap.xml"
 
-    assert p.title == ''
-    assert p.description == ''
+    assert p.title == ""
+    assert p.description == ""
     assert p.keywords == {}
     assert p.warnings == []
     assert p.links == []
 
+
 def test_analyze():
-    p = page.Page(url='https://www.sethserver.com/', base_domain='https://www.sethserver.com/')
+    p = page.Page(
+        url="https://www.sethserver.com/", base_domain="https://www.sethserver.com/"
+    )
     assert p.analyze()
